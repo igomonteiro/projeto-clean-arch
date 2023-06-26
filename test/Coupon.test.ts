fa95da38a035f399927c87442e4e9d1e4315d3fd
Igo Brasil Monteiro <igobrm@pop-os.localdomain>
2023-06-26 20:36:51 -0300

    test: add coupon tests

--- test/Coupon.test.ts
+++ test/Coupon.test.ts
@@ -1,6 +1,15 @@
 import Coupon from '../src/Coupon';
 
-test('Deve criar um cupom de desconto de 20%', () => {
-  const coupon = new Coupon('VALE20', 20);
-  expect(coupon).toBeTruthy();
+test('Deve criar um cupom de desconto válido', () => {
+  const coupon = new Coupon('VALE20', 20, new Date('2021-12-10'));
+  const today = new Date('2021-12-01');
+  const isValid = coupon.isValid(today);
+  expect(isValid).toBeTruthy();
+});
+
+test('Deve criar um cupom de desconto expirado', () => {
+  const coupon = new Coupon('VALE20', 20, new Date('2021-03-01'));
+  const today = new Date('2021-12-01');
+  const isExpired = coupon.isExpired(today);
+  expect(isExpired).toBeTruthy();
 });
